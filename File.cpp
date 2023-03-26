42e80a4bc563c4e320127303d7c8413ae8484cc3
Daniel Jelenici <djelenic@sfu.ca>
2023-03-25 19:36:49 -0700

    gets all dependecies for completed instructions

--- File.cpp
+++ File.cpp
@@ -25,6 +25,25 @@ void File::GetCompletedInstructions(){
         if (file->is_open()) {
         string line;
         // Read the file line by line
+        for(int i = 0 ; i < this->start_instruction; i++){
+            getline(*file, line);
+
+            vector<std::string> row;  // vector to hold the row data
+            istringstream tokenStream(line);  // create a stringstream from the line
+            string token;
+            // Split the line on the comma delimiter
+            while (getline(tokenStream, token, ',')) {
+                row.push_back(token);  // add the token to the row vector
+            }
+
+            Instruction instr( stoul(row.at(0), nullptr, 16), static_cast<InstructionType>(stoi(row.at(1))));
+            for(long unsigned int i = 2; i < row.size(); i++){
+                instr.addDependency(stoul(row.at(i), nullptr, 16));
+            }
+            pipeline.addToInstructions(instr);
+
+        }
+        /*
         while (getline(*file, line)) {
             vector<std::string> row;  // vector to hold the row data
             istringstream tokenStream(line);  // create a stringstream from the line
@@ -39,7 +58,7 @@ void File::GetCompletedInstructions(){
                 instr.addDependency(stoul(row.at(i), nullptr, 16));
             }
             pipeline.addToInstructions(instr);
-        }
+        }*/
     }
 
 }
